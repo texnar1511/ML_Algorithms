da4639474d8e85da642b5a249b12284b8f5da92a
texnar1511 <texnar1511@gmail.com>
2024-08-26 00:12:37 +0300

    improve KNNClassification

--- KNNClassification.py
+++ KNNClassification.py
@@ -1,106 +1,89 @@
+import numpy as np
 import pandas as pd
 from sklearn.datasets import make_classification
 
-X, y = make_classification(n_samples=1000, n_features=14, n_informative=10, random_state=42)
+class MyKNNClf():
+    
+    def __init__(self, k: int = 3, metric: str = 'euclidean', weight: str = 'uniform') -> None:
+        self.k: int = k
+        self.train_size: tuple = (0, 0)
+        self._metrics: dict = {'euclidean': self.Euclidean, 'chebyshev': self.Chebyshev, 'manhattan': self.Manhattan, 'cosine': self.Cosine}
+        self.metric = self._metrics[metric]
+        self.weight: str = weight
+        
+    def __str__(self) -> str:
+        ans = 'MyKNNClf class:'
+        for key in self.__dict__:
+            ans += f' {key}={self.__dict__[key]},'
+        return ans[:-1]
+    
+    def Euclidean(self, row: pd.Series, X: pd.DataFrame) -> pd.Series:
+        return ((X - row) ** 2).sum(axis = 1) ** 0.5
+    
+    def Chebyshev(self, row: pd.Series, X: pd.DataFrame) -> pd.Series:
+        return (X - row).abs().max(axis = 1)
+    
+    def Manhattan(self, row: pd.Series, X: pd.DataFrame) -> pd.Series:
+        return (X - row).abs().sum(axis = 1)
+    
+    def Cosine(self, row: pd.Series, X: pd.DataFrame) -> pd.Series:
+        return 1 - (X * row).sum(axis = 1) / (row ** 2).sum() ** 0.5 / (X ** 2).sum(axis = 1) ** 0.5
+    
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        self.X = X
+        self.y = y
+        self.train_size = X.shape
+        
+    def predict(self, X: pd.DataFrame) -> pd.Series:
+        ans = []
+        for index, row in X.iterrows():
+            neighbors = self.metric(row, self.X).sort_values().head(self.k)
+            indices = neighbors.index.tolist()
+            an = []
+            if self.weight == 'uniform':
+                an = int(self.y.iloc[indices].mean() >= 0.5) 
+            elif self.weight == 'rank':
+                c = np.array(self.y.iloc[indices])
+                r = np.array(range(1, self.k + 1))
+                an = int((1 / r[np.where(c == 1)]).sum() / (1 / r).sum() >= 0.5)
+            elif self.weight == 'distance':
+                c = np.array(self.y.iloc[indices])
+                d = np.array(neighbors)
+                an = int((1 / d[np.where(c == 1)]).sum() / (1 / d).sum() >= 0.5)
+            ans += [an]
+        return pd.Series(ans)
+    
+    def predict_proba(self, X: pd.DataFrame) -> pd.Series:
+        ans = []
+        for index, row in X.iterrows():
+            neighbors = self.metric(row, self.X).sort_values().head(self.k)
+            indices = neighbors.index.tolist()
+            an = []
+            if self.weight == 'uniform':
+                an = self.y.iloc[indices].mean()
+            elif self.weight == 'rank':
+                c = np.array(self.y.iloc[indices])
+                r = np.array(range(1, self.k + 1))
+                an = (1 / r[np.where(c == 1)]).sum() / (1 / r).sum()
+            elif self.weight == 'distance':
+                c = np.array(self.y.iloc[indices])
+                d = np.array(neighbors)
+                an = (1 / d[np.where(c == 1)]).sum() / (1 / d).sum()
+            ans += [an]
+        return pd.Series(ans)
+    
+    
+X, y = make_classification(n_samples=15, n_features=14, n_informative=10, random_state=42)
 X = pd.DataFrame(X)
 y = pd.Series(y)
 X.columns = [f'col_{col}' for col in X.columns]
 
-X_test, y_test = make_classification(n_samples=100, n_features=14, n_informative=10, random_state=42)
+X_test, y_test = make_classification(n_samples=15, n_features=14, n_informative=10, random_state=40)
 X_test = pd.DataFrame(X_test)
-y_test = pd.Series(y)
+y_test = pd.Series(y_test)
 X_test.columns = [f'col_{col}' for col in X_test.columns]
 
-class MyKNNClf:
-
-    def __init__(self, k = 3, metric = 'euclidean', weight = 'uniform'):
-        self.k = k
-        self.train_size = (0, 0)
-        self._metrics = {
-            'euclidean': self.Euclidean,
-            'chebyshev': self.Chebyshev,
-            'manhattan': self.Manhattan,
-            'cosine': self.Cosine
-            }
-        self.metric = self._metrics.get(metric, None)
-        self.weight = weight
-
-    def __str__(self):
-        ans = 'MyKNNClf class: '
-        for key in self.__dict__:
-            ans += f'{key}={self.__dict__[key]}, '
-        return ans[:-2]
-    
-    def fit(self, X: pd.DataFrame, y: pd.Series):
-        self.X_train = X
-        self.y_train = y
-        self.train_size = X.shape
-
-    def predict(self, X_test: pd.DataFrame) -> pd.Series:
-        ans = []
-        for i in range(X_test.shape[0]):
-            Dist = self.metric(self.X_train, X_test.iloc[i]).sort_values().iloc[:self.k]
-            Dist.name = 'distance'
-            Classes = self.y_train.iloc[Dist.index]
-            Classes.name = 'class'
-            Rank = pd.Series(range(1, self.k + 1), name = 'rank', index = Dist.index)
-            DCR = pd.concat([Dist, Classes, Rank], axis = 1)
-            DCR['distance'] = 1 / DCR['distance']
-            DCR['rank'] = 1 / DCR['rank']
-            value = 0
-            if self.weight == 'uniform':
-                value = 1 if 2 * Classes.sum() >= len(Classes) else 0
-            elif self.weight == 'distance':
-                value = 1 if 2 * DCR[DCR['class'] == 1]['distance'].sum() / DCR['distance'].sum() >= 1 else 0
-            elif self.weight == 'rank':
-                value = 1 if 2 * DCR[DCR['class'] == 1]['rank'].sum() / DCR['rank'].sum() >= 1 else 0
-            else:
-                pass
-            ans += [value]
-        return pd.Series(ans)
-    
-    def predict_proba(self, X_test: pd.DataFrame) -> pd.Series:
-        ans = []
-        for i in range(X_test.shape[0]):
-            Dist = self.metric(self.X_train, X_test.iloc[i]).sort_values().iloc[:self.k]
-            Dist.name = 'distance'
-            Classes = self.y_train.iloc[Dist.index]
-            Classes.name = 'class'
-            Rank = pd.Series(range(1, self.k + 1), name = 'rank', index = Dist.index)
-            DCR = pd.concat([Dist, Classes, Rank], axis = 1)
-            DCR['distance'] = 1 / DCR['distance']
-            DCR['rank'] = 1 / DCR['rank']
-            value = 0.0
-            if self.weight == 'uniform':
-                value = Classes.sum() / len(Classes)
-            elif self.weight == 'distance':
-                value = DCR[DCR['class'] == 1]['distance'].sum() / DCR['distance'].sum()
-            elif self.weight == 'rank':
-                value = DCR[DCR['class'] == 1]['rank'].sum() / DCR['rank'].sum()
-            else:
-                pass
-            ans += [value]
-        return pd.Series(ans)
-    
-    def Euclidean(self, X: pd.DataFrame, y: pd.Series) -> pd.Series:
-        return ((X - y) ** 2).sum(axis = 1) ** (1 / 2)
-    
-    def Chebyshev(self, X: pd.DataFrame, y: pd.Series) -> pd.Series:
-        return (X - y).abs().max(axis = 1)
-    
-    def Manhattan(self, X: pd.DataFrame, y: pd.Series) -> pd.Series:
-        return (X - y).abs().sum(axis = 1)
-    
-    def Cosine(self, X: pd.DataFrame, y: pd.Series) -> pd.Series:
-        return 1 - (X * y).sum(axis = 1) / (y ** 2).sum() ** (1 / 2) / (X ** 2).sum(axis = 1) ** (1 / 2)
-
-
-        
-    
-#a = MyKNNClf(metric = 'euclidean', weight = 'rank')
-#print(a)
-
-#a.fit(X, y)
-#print(a.predict(X_test).sum())
-#print(a.predict_proba(X_test).sum())
-
+a = MyKNNClf(weight = 'distance')
+a.fit(X, y)
+print(a.predict(X_test))
+print(a.predict_proba(X_test))
